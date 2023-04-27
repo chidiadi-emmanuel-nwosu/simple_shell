83d943b55c63b05e42a038b845995ad8d3f8b560
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-04-27 12:08:28 +0100

    removed isatty check

--- main.c
+++ main.c
@@ -17,10 +17,10 @@ int main(__attribute__((unused)) int ac, char **av)
 {
 	int hist = 0;
 	char *prog = av[0];
-
-	if (!isatty(STDIN_FILENO))
-		_non_isatty(prog, &hist);
-
+/*
+ *	if (!isatty(STDIN_FILENO))
+ *	_non_isatty(prog, &hist);
+ */
 	if (ac > 1)
 		file_input(av, prog, &hist);
 
